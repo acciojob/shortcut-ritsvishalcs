00aa0cf92143701d9b92878924ff459f35e127a9
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-10 13:45:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function shortcut(s1, s2) {
-  // your code here
-	 if (str1.length === 0 || str2.length === 0) {
+function shortcut(str1, str2) {
+  if (!str1 || !str2) {
     return '';
   }
   return str1[0] + str2[0];
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+const str1 = prompt("Enter first word:");
+const str2 = prompt("Enter second word:");
+
+const result = shortcut(str1, str2);
+alert(result);
+
